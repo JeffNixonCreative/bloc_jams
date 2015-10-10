f95df3b5f642fabc935bfe69674fcf44150d85d8
Jeff Nixon <jeffnixoncreative@gmail.com>
2015-10-09 17:36:08 -0700

    checkpoint 30 complete

--- scripts/album.js
+++ scripts/album.js
@@ -1,95 +1,108 @@
-// #1: alocate values to a new, accessible object
- var albumPicasso = {
-                                                                        // album-view-details...
-     name: 'The Colors',                                                    // ...TITLE element "The Colors"
-     artist: 'Pablo Picasso',                                               // ...ARTIST element "Pablo Picasso"
-     label: 'Cubism',                                                       // ...RELEASE element Cubism
-     year: '1881',                                                          // ...YEAR element 1881 ????? How do these coe together???
-     albumArtUrl: 'assets/images/album_covers/01.png',                  // album-cover-art element (picture)
-                                                                        // album-view-song-list
-     songs: [                                                               // sub-object (array)
-         { name: 'Blue', length: '4:26' },                                      // row 1
-         { name: 'Green', length: '3:14' },                                     // row 2
-         { name: 'Red', length: '5:01' },                                       // row 3
-         { name: 'Pink', length: '3:21'},                                       // row 4
-         { name: 'Magenta', length: '2:15'}                                     // row 5
-     ]
- };
- 
-// #1: alocate values to a new, accessible object
- var albumMarconi = {
-                                                                        // album-view-details...
-     name: 'The Telephone',                                                 // title element "The Colors"
-     artist: 'Guglielmo Marconi',                                           // artist name element "Pablo Picasso"
-     label: 'EM',                                                           // record label element "???1881?? Cubism
-     year: '1909',                                                          // year element 1881
-     albumArtUrl: 'assets/images/album_covers/20.png',                  // album-cover-art element (picture)
-                                                                        // album-view-song-list TABLE
-     songs: [                                                               // sub-object (array)
-         { name: 'Hello, Operator?', length: '1:01' },                          // row 1
-         { name: 'Ring, ring, ring', length: '5:01' },                          // row 2
-         { name: 'Fits in your pocket', length: '3:21'},                        // row 3
-         { name: 'Can you hear me now?', length: '3:14' },                      // row 4
-         { name: 'Wrong phone number', length: '2:15'}                          // row 5
-     ]
- };
+//   PICASSO   //
+var albumPicasso = {
+    
+    name: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
 
-// #2: alocate table element to a new, accessible variables
-var createSongRow = function(songNumber, songName, songLength) {          // pass arguments from setCurrentAlbum function
-     
-     var template =                                                         // new variable determining table format
-        '<tr class="album-view-song-item">'                                     // calling the TABLE view
-      + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'                    // assigning argOne as the first column
-      + '  <td class="song-item-title">' + songName + '</td>'                       // assigning argTwo as the second column
-      + '  <td class="song-item-duration">' + songLength + '</td>'                  // assigning argThree as the second column
-      + '</tr>'                                                                 // enging the TABLE view
-      ;                                                                     // end of object
- 
-     return template;                                                       // make new object accessible
- 
- };
- 
- var setCurrentAlbum = function(album) {                                    // pass argument albumPicasso from event handler (onload)
-
-// #4: create variable with node elements to pass the values to
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];                 // new variable with value of node element
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];               // new variable with value of node element
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];    // new variable with value of node element
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];                  // new variable with value of node element
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];          // new variable with value of node element
-
-// #5: set the values of the new variable as the first child of each table node (column) as the arguements individual key
-     albumTitle.firstChild.nodeValue = album.name;                                // albumTitle as firstChild arg.name (key)
-     albumArtist.firstChild.nodeValue = album.artist;                             // albumArtist element as firstChild arg.name(key)
-     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;      // albumReleaseInfo as firstChild value of arg.year (key)
-     albumImage.setAttribute('src', album.albumArtUrl);                           // albumImage attribute as image src
-
-// #6: clear the table to be sure it is empty before passing in values to each node
-     albumSongList.innerHTML = '';
- 
-// #7: take in the arguments "song" key (in this case its an object) and loop it, passsing the values to new variable (TABLE element) 
-     for (i = 0; i < album.songs.length; i++) {                              // separate temblate argument object index/element
-         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].name, album.songs[i].length); // pass index/elements to table w/#
-     }
+    songs: [
+        { name: 'Blue', length: '4:26' },
+        { name: 'Green', length: '3:14' },
+        { name: 'Red', length: '5:01' },
+        { name: 'Pink', length: '3:21'},
+        { name: 'Magenta', length: '2:15'}
+    ]
 };
 
 
 
+//   MARCONI   //
+var albumMarconi = {
+
+    name: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+     
+    songs: [
+        { name: 'Hello, Operator?', length: '1:01' },
+        { name: 'Ring, ring, ring', length: '5:01' },
+        { name: 'Fits in your pocket', length: '3:21'},
+        { name: 'Can you hear me now?', length: '3:14' },
+        { name: 'Wrong phone number', length: '2:15'}
+    ]
+ };
 
 
-//CREATED IN ASSIGNMENT 27
-var child = document.getElementsByClassName('album-view-title')[0];
-var noParent = document.querySelector('html');
+//   TEMPLATE  //
+    //    • Song Item
+    //    • Song Number
+    //    • Song Title
+    //    • Song Duration
+var createSongRow = function(songNumber, songName, songLength) {
+    var template =
+        '<tr class="album-view-song-item">' + '<td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>' + '  <td class="song-item-title">' + songName + '</td>' + '  <td class="song-item-duration">' + songLength + '</td>' + '</tr>';
+    
+//    return template;
+    return $(template);
+    
+ 
+};
 
-//CREATED IN CHECKPOINT 27
+//  POPULATE THE TEMPLATE ALBUM PASSED IN AS ARGUMENT WITH PICASSO AS DEFAULT ON LOAD
+var setCurrentAlbum = function(album) {
+
+//    var albumTitle = document.getElementsByClassName('album-view-title')[0];
+//    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+//    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+//    var albumImage = document.getElementsByClassName('album-cover-art')[0];
+//    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+    
+    var $albumTitle = $('.album-view-title');
+    var $albumArtist = $('.album-view-artist');
+    var $albumReleaseInfo = $('.album-view-release-info');
+    var $albumImage = $('.album-cover-art');
+    var $albumSongList = $('.album-view-song-list');
+
+//    albumTitle.firstChild.nodeValue = album.name;
+//    albumArtist.firstChild.nodeValue = album.artist;
+//    albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
+//    albumImage.setAttribute('src', album.albumArtUrl);
+
+    $albumTitle.text(album.name);
+    $albumArtist.text(album.artist);
+    $albumReleaseInfo.text(album.year + ' ' + album.label);
+    $albumImage.attr('src', album.albumArtUrl);
+    
+//    albumSongList.innerHTML = '';
+    $albumSongList.empty();
+
+    for (i = 0; i < album.songs.length; i++) {
+//        albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].name, album.songs[i].length);
+        var $newRow = createSongRow(i + 1, album.songs[i].name, album.songs[i].length);
+        $albumSongList.append($newRow);
+    }
+};
+
+
+//  NEW VARIABLE with the value of the DOM element album-view-title to test the findParentByClass function
+//var child = document.getElementsByClassName('album-view-title')[0];
+
+//  NEW VARIABLE with the value of the entire html document as its value to test the findParentByClass function
+//var noParent = document.querySelector('html');
+
+
+
+// FUNCTION THAT SAYS for the first agrument (element) passed in, provide the parentElement of the argument as the value to a new variable that, if-while its value is className, and className is not equal to the second argument (targetClass), we pass the parentElement back to the variable as its value. But, if the className is equal to the the second argument (targetClass), return the the original value of the currentParent variable. Otherwise, alert the user that "no parent with that class name was found". Else, if none of these conditions are met, alert the user that there was "No parent found".
 var findParentByClassName = function(element, targetClass) {
     var currentParent = element.parentElement;
-//CREATED IN ASSIGNMENT 27    
     if (currentParent) {
         while (currentParent.className && currentParent.className != targetClass) {
             currentParent = currentParent.parentElement;
-        } 
-        
+        }
         if (currentParent.className == targetClass) {
             return currentParent;
         } else {
@@ -100,43 +113,50 @@ var findParentByClassName = function(element, targetClass) {
     }
 };
 
-findParentByClassName(child, 'album-view');
+
+
+// TEST the function to see if it works y passing 'child' in as the first argument and 'album-view' in as the second argument.
+//findParentByClassName(child, 'album-view');
     
 
-//CREATED IN CHECKPOINT 27
+
 var getSongItem = function(element) {
     
     switch (element.className) {
         case 'album-song-button':
         case 'ion-play':
         case 'ion-pause':
-            return findParentByClassName(element, 'song-item-number');
-        
+            return findParentByClassName(element, 'song-item-number');    
         case 'album-view-song-item':
             return element.querySelector('.song-item-number');
-        
         case 'song-item-title':
         case 'song-item-duration':
             return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
-        
         case 'song-item-number':
             return element;
-        
         default:
             return;
     }
     
 };
 
-//CREATED IN CHECKPOINT 27
+
+
+//  TABLE ROW STATE clickHandler function that changes the content of the song number element between Pause, Play and Number. It receives an argument as a result of a row being clicked on (window.onload click event handeler set to each row of the table), creates a new variable (songItem) that holds the value from the getSongItem function ( TBD ) with the argument (targetElement) as the argument.
 var clickHandler = function(targetElement) {
     var songItem = getSongItem(targetElement);
+
+//  And if currentlyPlayingSong is equal to null (meaning, nothing is playing), clear the the content of the new variable (songItem) and replace it with the value of pauseButtonTemplate (pause button).
     if (currentlyPlayingSong === null) {
          songItem.innerHTML = pauseButtonTemplate;
          currentlyPlayingSong = songItem.getAttribute('data-song-number');
+
+// But if the currentlyPlayingSong is equal to data-song-number class (displaying the song number, rather tham pause or play), clear the content of songItem and replace it with the Play button (play-button-template) while returning the curently playing song to null (stopping the song from continuing to play)
      } else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
          songItem.innerHTML = playButtonTemplate;
          currentlyPlayingSong = null;
+
+// But if the currentlyPlayingSong is not equal to data-song-number class (displaying something other than the song number), create a new variable (currentlyPlayingSongElement) with the value of an array (songNumber + currentlyPlayingSong), then clearing the content of currentlyPlayingSongElement and replacing it with the the song number (data-song-number). Then clear the songItem content and replace it with the pause button. Then pass the song number to the currentlyPlayingSong variable.
      } else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
          var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
          currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
@@ -145,72 +165,49 @@ var clickHandler = function(targetElement) {
      }
 };
 
-//CREATED IN CHECKPOINT 27
-// Elements we'll be adding listeners to
+
+
+//  GLOBAL VARIABLES: These are elements we'll be adding listeners to.
+//  album-view-song-LIST.
 var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+//  album-view-song-ITEM.
 var songRows = document.getElementsByClassName('album-view-song-item');
- 
 
 
 
-
-
-//CREATED IN PREVIOUS CHECKPOINT
-// Album button templates
+//  PLAY/PAUSE BUTTON STATES
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
-
-
-
-
-
-
-// CREATED IN CHECKPOINT 27
-// #3: trigger the data to populate onload
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 
-// Store state of playing songs
+
+// DEFAULT STATE OF THE CURRENLY PLAYING SONG as empty/null
 var currentlyPlayingSong = null;
 
 
 
-
-
-
-
-
-
- window.onload = function() {                                               
-     // call setCurretAlbum function with argument   
-     setCurrentAlbum(albumPicasso);                                         
+//  WINDOW ONLOAD function that says when the window loads, the default album to display will be Picasso. Then, associate a mouseover event listener function that: 
+ window.onload = function() { 
+     setCurrentAlbum(albumPicasso);
      songListContainer.addEventListener('mouseover', function(event) {
 
-         // Only target individual song rows during event delegation
+//  if the events target parent elements classname is album-view-song-item, create a variable (songItem) with the value of the element that triggered the event (songListContainer which is the album-view-song-list eement, which is the entire table, right ???????). And if the value of songItem's child element that displays the song number (data-song-number) is not equal to currentlyPlayingSong clear its content and replace it with the Play button (playButtonTemplate).
          if (event.target.parentElement.className === 'album-view-song-item') {
-             // Change the content from the number to the play button's HTML
-//             event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
-// CREATED IN CHECKPOINT 27
-// BLOC's SOLUTION
              var songItem = getSongItem(event.target);
              if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
                  songItem.innerHTML = playButtonTemplate;
              }
-         }
+         } 
+
+//  Then, loop through songRows (which holds the value of album-view-song-item...the entire row) and listen for the event of the mouse leaving. Create a new variable with the value of the element that triggered the event (which ever row that may be) and another variable with the value of that rows song number (data-song-number), clear its content and return its value to the row number (rather than play or pause). Add a click event listener that passes the element that triggered the event as an argument to the clickHandler function.
          
          for (i = 0; i < songRows.length; i++) {
+
              songRows[i].addEventListener('mouseleave', function(event) {
-                 // CREATED IN CHECKPOINT 27
-                 // #1: cached the song item that we're leaving in a variable
                  var songItem = getSongItem(event.target);
                  var songItemNumber = songItem.getAttribute('data-song-number');
-                 // #2: check item mouse is leaving is NOT current song, and only change content if it isn't
                  if (songItemNumber !== currentlyPlayingSong) {
                      songItem.innerHTML = songItemNumber;
                  }
-
-// JEFF's SOLUTION                 
-//                 else {
-//                     songItem.innerHTML = pauseButtonTemplate;
-//                 }
              });
              songRows[i].addEventListener('click', function(event) {
              clickHandler(event.target);
--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,25 @@
-var collectionItemTemplate =
+
+//  VANILLA  //
+//var collectionItemTemplate =
+//    '<div class="collection-album-container column fourth">'
+//    + '<img src="assets/images/album_covers/01.png"/>'
+//    + '  <div class="collection-album-info caption">'
+//    + '    <p>'
+//    + '      <a class="album-name" href="/album.html"> The Colors </a>'  
+//    + '      <br/>'
+//    + '      <a href="/album.html"> Pablo Picasso </a>'
+//    + '      <br/>'
+//    + '      X songs'
+//    + '      <br/>'
+//    + '    </p>'
+//    + '  </div>'
+//    '</div>';
+
+
+//  REFACTORED  //
+//As a jQuery naming convention, we rename the function by adding a verb to the beginning of the variable (buildCollectionItemTemplate) name an establish it as a function that creates a new variable (template) with the content of any given albums associaited elements. Then, we return that variable as a jquery object just in case we later end up using jQuery methods.
+var buildCollectionItemTemplate = function() {
+    var template =
     '<div class="collection-album-container column fourth">'
     + '<img src="assets/images/album_covers/01.png"/>'
     + '  <div class="collection-album-info caption">'
@@ -11,21 +32,35 @@ var collectionItemTemplate =
     + '      <br/>'
     + '    </p>'
     + '  </div>'
-    '</div>';
+    '</div>'
+    ;
+    
+    return $(template);
+};
 
 
- 
- window.onload = function() {
-     
-     // #1
-     var collectionContainer = document.getElementsByClassName('album-covers')[0].childNodes[1];
-     // #2
-     collectionContainer.innerHTML = '';
-     // #3
-     for (var i = 0; i < 12; i++) {
-         collectionContainer.innerHTML += collectionItemTemplate;
-     }
- }
- 
- 
- 
+// VANILLA  //
+// window.onload = function() {
+//     
+//     // #1
+//     var collectionContainer = document.getElementsByClassName('album-covers')[0].childNodes[1];
+//     // #2
+//     collectionContainer.innerHTML = '';
+//     // #3
+//     for (var i = 0; i < 12; i++) {
+//         collectionContainer.innerHTML += collectionItemTemplate;
+//     }
+// }
+
+
+//  REFACTORED  //
+//When the window loads, we will pass in a function that creates a new variable with the value of .album-covers element. Then empty its content and looping through it passing the value of each child element of the .album-covers object to a new variable (newThumbnail) containing the value of our template (bildColectionItemTemplate). This populate the template with the information of the first album listed.
+//The ".append" method inserts content, specified by the parameter, to the end of each element in the set of matched elements, returning either an HTML String, a DOM element or jQuery object as either a string or an array depending on the set structure of object it calls????
+$(window).load(function() {
+    var $collectionContainer = $('.album-covers');
+    $collectionContainer.empty();
+    for (var i = 0; i < 12; i++) {
+        var $newThumbnail = buildCollectionItemTemplate();
+        $collectionContainer.append($newThumbnail);
+    }
+});
